f13bea5f93d16d3b09d1c70fec512bd0c93f9de2
Bhoomik Gupta <gbhoomik@gmail.com>
2025-04-21 01:02:52 +0530

    Add initial project files including Cargo.toml, Cargo.lock, .gitignore, and main.rs with enum and struct examples

---
+++ enum_pattern_matching/src/main.rs
@@ -0,0 +1,78 @@
+enum Pet {
+    Dog,
+    Cat,
+    Fish,
+}
+impl Pet {
+    fn sound(&self) -> &str {
+        match self {
+            Pet::Dog => "Woof",
+            Pet::Cat => "Meow",
+            Pet::Fish => "Blub",
+        }
+    }
+}
+
+enum IpAddrKind{
+    V4,
+    V6,
+}
+
+struct IpAddr{
+    kind: IpAddrKind,
+    address: String,
+}
+
+// enum Option<T> {
+//     Some(T),
+//     None,
+// }
+fn main() {
+    let dog = Pet::Dog;
+    println!("Dog sound: {}", dog.sound());
+    let cat = Pet::Cat;
+    println!("Cat sound: {}", cat.sound());
+    let home = IpAddr{
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+    let loopback = IpAddr{
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+    let some_number = Some(5);
+    let some_string = Some("Hello");
+    let absent_number: Option<i32> = None;
+    println!("Some number: {:?}", some_number);
+    println!("Some string: {:?}", some_string);
+    println!("Absent number: {:?}", absent_number);
+    let x:i32 = 5;
+    let y: Option<i32> = Some(10);
+    // let sum = x + y; // This will cause a compile-time error
+    //no implementation for `i32 + Option<i32>
+    let five = Some(5);
+    let six = plus_one(five);
+    println!("Five plus one: {}", six);
+    let none = plus_one(None);
+    println!("None plus one: {}", none);
+    what_pet("dog");
+    what_pet("cat");
+    what_pet("fish");
+    what_pet("bird"); // This will panic at runtime
+
+}
+fn plus_one(x: Option<i32>) -> i32 {
+    match x {
+        Some(i) => i + 1,
+        None => None,
+    }
+}
+
+fn what_pet(input: &str) -> Pet {
+    match input {
+        "dog" => println!("Dog"),
+        "cat" => println!("Cat"),
+        "fish" => println!("Fish"),
+        _ => panic!("Unknown pet!"),
+    }
+}
