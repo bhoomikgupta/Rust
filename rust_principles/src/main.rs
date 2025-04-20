e67ef71ecc471e6029086f51ac847031aabe701f
Bhoomik Gupta <gbhoomik@gmail.com>
2025-04-20 18:07:54 +0530

    Refactor main function to include ownership and borrowing examples

--- rust_principles/src/main.rs
+++ rust_principles/src/main.rs
@@ -24,9 +24,29 @@ fn main() {
     let s = String::from("hello");
     takes_ownership(s); // give ownership of s to the function
     // println!("{}", s); // This will cause a compile-time error
+    let val1 =1;
+    make_copy(val1); // copy val1 to the function
+    println!("{}", val1); // This will work
+    let str1:String = give_ownership();
+    println!("{}", str1); // This will work
+    let str3:String = take_and_give_ownership(str1);
+    println!("{}", str3); // This will work
+    let str4 = String::from("Bhoomik");
+    let mut str5:String;
+    // loop{
+    //     //str5=str4; //value moved here, in previous iteration of loop
+    //     str5 = str4.clone(); //value copied here, in previous iteration of loop
+    // }
+    let mut str6 = String::from("hello");
+    change_string(&mut str6); // pass a reference to s
+    println!("{}",str6);
 }
 // s is dropped here
 //var is dropped after main
+fn change_string(some_string: &mut String) {
+    some_string.push_str(" world"); // This will cause a compile-time error
+}
+
 fn takes_ownership(s: String) {
     let strin = s;
     println!("{}", strin);
@@ -37,3 +57,10 @@ fn make_copy(x: i32) {
     let val1 = x;
     println!("{}", val1);
 }
+
+fn give_ownership() -> String {
+    "given".to_string()
+}
+fn take_and_give_ownership(s: String) -> String {
+    s
+ }
