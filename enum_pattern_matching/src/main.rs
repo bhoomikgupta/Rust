33773b06d62b9dffa0e4da0dd04199b52093fecd
Bhoomik Gupta <gbhoomik@gmail.com>
2025-04-21 23:53:42 +0530

    Refactor main function to improve variable naming and update plus_one function to return Option<i32>

--- enum_pattern_matching/src/main.rs
+++ enum_pattern_matching/src/main.rs
@@ -32,11 +32,11 @@ fn main() {
     println!("Dog sound: {}", dog.sound());
     let cat = Pet::Cat;
     println!("Cat sound: {}", cat.sound());
-    let home = IpAddr{
+    let _home = IpAddr{
         kind: IpAddrKind::V4,
         address: String::from("127.0.0.1"),
     };
-    let loopback = IpAddr{
+    let _loopback = IpAddr{
         kind: IpAddrKind::V6,
         address: String::from("::1"),
     };
@@ -46,29 +46,29 @@ fn main() {
     println!("Some number: {:?}", some_number);
     println!("Some string: {:?}", some_string);
     println!("Absent number: {:?}", absent_number);
-    let x:i32 = 5;
-    let y: Option<i32> = Some(10);
+    let _x:i32 = 5;
+    let _y: Option<i32> = Some(10);
     // let sum = x + y; // This will cause a compile-time error
     //no implementation for `i32 + Option<i32>
     let five = Some(5);
     let six = plus_one(five);
-    println!("Five plus one: {}", six);
+    println!("Five plus one: {:?}", six);
     let none = plus_one(None);
-    println!("None plus one: {}", none);
+    println!("None plus one: {:?}", none);
     what_pet("dog");
     what_pet("cat");
     what_pet("fish");
     what_pet("bird"); // This will panic at runtime
 
 }
-fn plus_one(x: Option<i32>) -> i32 {
+fn plus_one(x: Option<i32>) -> Option<i32> {
     match x {
-        Some(i) => i + 1,
+        Some(i) => Some(i + 1),
         None => None,
     }
 }
 
-fn what_pet(input: &str) -> Pet {
+fn what_pet(input: &str) {
     match input {
         "dog" => println!("Dog"),
         "cat" => println!("Cat"),
