f6b039541516c908571c44bcfc742fabb885e95b
Bhoomik Gupta <gbhoomik@gmail.com>
2025-04-22 19:15:30 +0530

    Add functionality to remove and test activities; update visibility of update_activity function

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -27,3 +27,18 @@ fn lets_add_task(){
     items_completed::remove_activity(); //imported function
     test(); //imported function
 }
+// PS C:\Users\gupta\study\Rust\lib> cargo-modules structure
+
+// crate lib
+// ├── fn lets_add_task: pub(crate)
+// ├── mod list: pub(crate)
+// │   └── struct Tasks: pub
+// └── mod things_todo: pub(crate)
+//     ├── fn add_activity: pub
+//     ├── mod items_completed: pub
+//     │   ├── fn remove_activity: pub
+//     │   ├── mod test: pub
+//     │   │   └── fn test: pub
+//     │   └── fn update_activity: pub(self)
+//     ├── fn marked_complete: pub
+//     └── fn update_activity: pub
--- lib/src/things_todo/items_completed.rs
+++ lib/src/things_todo/items_completed.rs
@@ -1,5 +1,5 @@
 pub fn remove_activity(){}
-pub fn update_activity(){}
+fn update_activity(){}
 pub mod test{
     pub fn test(){
         println!("Test function in items_completed module");
